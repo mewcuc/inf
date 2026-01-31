21e74ea1e5d43f1cae1ae640d3bc09032ecaa26b
mewcuc <nikitagiden@gmail.com>
2026-01-31 20:17:03 +0300

    Update 25делит.py

--- 25делит.py
+++ 25делит.py
@@ -30,3 +30,28 @@ for i in range(1, 20):
 # либо вот такой вариант еще
 def sigma(n):
     return n > 1 and all(n % d != 0 for d in range(2, int(n**0.5) + 1))
+
+''' ------------------------------------------------- '''
+
+# Вариации нахождения прстых делителей (проги выовдит true, если число простое
+
+def f(n):
+    res = []
+    for d in range(1, int(n**0.5)+1):
+        if n % d == 0:
+            res.append(d)
+            res.append(n // d)
+    if len(res) == 2: return True
+    else: return False
+
+def pr(n):
+    res = []
+    for d in range(1, int(n**0.5)+1):
+        if n % d == 0:
+            return True
+    return False
+
+def sigma(n):
+    return n > 1 and all(n % i != 0 for i in range(2, int(n**0.5)+1))
+
+print(f(17), pr(17))
